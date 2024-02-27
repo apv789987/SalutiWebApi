21b1f880cb442fd8619427389b6f5760b132c3c5
andrea.paravani <andrea.paravani@SPXRMW8629>
2024-02-27 16:19:52 +0100

    Aggiungere i file di progetto.

---
+++ SalutiWebApi/Controllers/InfoMsg.cs
@@ -0,0 +1,14 @@
+﻿namespace SalutiWebApi.Controllers
+{
+    public class InfoMsg
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+
+        public InfoMsg(string Code, String Message)
+        {
+            this.Code = Code;
+            this.Message = Message;
+        }
+    }
+}
---
+++ SalutiWebApi/Controllers/SalutiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalutiWebApi.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/saluti")]
+    public class SalutiController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult TestConnex()
+        {
+            return Ok(new InfoMsg("101", $"Saluti sono la tua prima web api csharp in Azure App Services"));
+        }
+
+        [HttpGet("{Nome}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<InfoMsg> getSaluti2(string Nome)
+        {
+            return Ok(new InfoMsg("101", $"Saluti {Nome}, sono la tua web api csharp in Azure App Services"));
+        }
+
+        [HttpGet("{Nome}/{version}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<InfoMsg> getSaluti3(string Nome, string version)
+        {
+            return Ok(new InfoMsg("101", $"Saluti {Nome}, sono la versione {version} della web api"));
+        }
+    }
+}
